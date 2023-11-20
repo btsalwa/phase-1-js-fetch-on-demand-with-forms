f80a84c02f38b7bb5358bd780c24caeba781e136
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-11-20 17:12:52 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ const init = () => {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+const inputForm = document.querySelector('form');
+
+inputForm.addEventListener('submit', (event) => {
+
+});
+
+const init = () => {
+    const inputForm = document.querySelector('form');
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+    });
+}
+
